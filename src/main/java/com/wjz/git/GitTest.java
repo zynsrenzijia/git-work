ecca5c346fa3a620e430ea6651c18f0a87a5def3
Ryan <709923869@qq.com>
2021-04-27 21:38:58 +0800

    hot-fix commit1

--- src/main/java/com/wjz/git/GitTest.java
+++ src/main/java/com/wjz/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello github2");
         System.out.println("hello github3");
+        System.out.println("hello github4");
     }
 }
