7f6adc7f8c059dcbcf9b801fd4689c076d6aa0db
Ryan <709923869@qq.com>
2021-04-27 21:23:14 +0800

    first commit

---
+++ src/main/java/com/wjz/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wjz.git;
+
+/**
+ * @author Ryan
+ * @create 2021-04-27 21:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
