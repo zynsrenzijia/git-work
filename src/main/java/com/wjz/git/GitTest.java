fcfc3d7840f88b459c1cd873c46f5b4e9c8b966d
Ryan <709923869@qq.com>
2021-04-27 22:10:26 +0800

    push commit2

--- src/main/java/com/wjz/git/GitTest.java
+++ src/main/java/com/wjz/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello github4");
         System.out.println("我叶良辰很是不爽!");
         System.out.println("我赵日天很是不服!");
+        System.out.println("push test!!!");
     }
 }
