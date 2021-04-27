8f4e4b62dcf4bf45f48ab2d67f2b7fe17fc3266d
Ryan <709923869@qq.com>
2021-04-27 21:27:13 +0800

    third commit

--- src/main/java/com/wjz/git/GitTest.java
+++ src/main/java/com/wjz/git/GitTest.java
@@ -7,6 +7,7 @@ package com.wjz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello github");
+        System.out.println("hello github2");
+        System.out.println("hello github3");
     }
 }
